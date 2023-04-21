e1dc3e19932dbb351d75c27650487c7aa212757d
kagan.meric <kagan.meric@enuygun.com>
2023-04-22 01:53:48 +0300

    add BeanController.java

---
+++ src/main/java/mkaganm/springboottrial/bean/controller/BeanController.java
@@ -0,0 +1,21 @@
+package mkaganm.springboottrial.bean.controller;
+
+import mkaganm.springboottrial.bean.config.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BeanController {
+
+    @Autowired
+    BeanConfig beanConfig;
+
+    // * http://localhost:8080/bean/beanDto
+    @GetMapping("/bean/beanDto")
+    @ResponseBody
+    public String getBeanDto() {
+        return beanConfig.beanDto() + "";
+    }
+}
