8de5e5ea873bb787ed79aefc1cd3e03c67547f36
kagan.meric <mustafakaganmeric@gmail.com>
2023-04-22 03:49:12 +0300

    junit to testng

--- src/test/java/mkaganm/springboottrial/SpringBootTrialApplicationTests.java
+++ src/test/java/mkaganm/springboottrial/SpringBootTrialApplicationTests.java
@@ -1,6 +1,6 @@
 package mkaganm.springboottrial;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
