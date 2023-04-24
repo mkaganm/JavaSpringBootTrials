45ab20e301feac4c1a6ce9ba5e02d995948dff8a
kagan.meric <mustafakaganmeric@gmail.com>
2023-04-24 09:17:52 +0300

    update ThymeleafController.java

--- src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
+++ src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
@@ -2,7 +2,6 @@ package mkaganm.springboottrial.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ThymeleafController {
