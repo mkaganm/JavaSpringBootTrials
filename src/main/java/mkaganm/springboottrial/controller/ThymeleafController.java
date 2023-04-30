aed8a5dc64939672041f186fae949afc7f9a402d
kagan.meric <mustafakaganmeric@gmail.com>
2023-04-30 17:39:28 +0300

    update

--- src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
+++ src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
@@ -17,7 +17,16 @@ public class ThymeleafController {
     // * http://localhost:8080/thymeleaf2
     @GetMapping("/thymeleaf2")
     public String getThymeleaf2(Model model) {
-        model.addAttribute("key_model", "model value");
+        model.addAttribute("key_model1", "model value1");
+        model.addAttribute("key_model2", "model value2");
         return "thymeleaf2";
+    }
+
+    // * http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf3(Model model) {
+        model.addAttribute("key_model1", "model value1");
+        model.addAttribute("key_model2", "model value2");
+        return "thymeleaf_file/thymeleaf3";
     }
 }
