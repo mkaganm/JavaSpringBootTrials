c75290253d07417bf43df2a5762e4c66e81a6269
kagan.meric <mustafakaganmeric@gmail.com>
2023-04-30 17:11:49 +0300

    update

--- src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
+++ src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
@@ -1,14 +1,23 @@
 package mkaganm.springboottrial.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThymeleafController {
 
     // * http://localhost:8080/thymeleaf_
+    // @ResponseBody
     @GetMapping("/thymeleaf_")
-    public String getThymeleaf_(){
+    public String getThymeleaf_() {
         return "thymeleaf_";
+    }
+
+    // * http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2(Model model) {
+        model.addAttribute("key_model", "model value");
+        return "thymeleaf2";
     }
 }
