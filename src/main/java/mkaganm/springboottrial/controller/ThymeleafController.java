a755645cac37c52eb2563f5604695cf115898fa3
kagan.meric <mustafakaganmeric@gmail.com>
2023-05-02 02:09:41 +0300

    update

--- src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
+++ src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
@@ -1,8 +1,12 @@
 package mkaganm.springboottrial.controller;
 
+import mkaganm.springboottrial.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ThymeleafController {
@@ -36,5 +40,39 @@ public class ThymeleafController {
         model.addAttribute("key_model1", "model value1");
         model.addAttribute("key_model2", "model value2");
         return "thymeleaf4";
+    }
+
+    // * http://localhost:8080/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5(Model model) {
+
+        model.addAttribute("key_model1", "text1");
+        model.addAttribute("key_model2", "text2");
+
+        ProductDto productDto = ProductDto.builder()
+                .productId(0L)
+                .productName("Product Name")
+                .productPrice(2500)
+                .build();
+
+
+        model.addAttribute("key_model3", productDto);
+        return "thymeleaf5";
+    }
+
+
+    // * http://localhost:8080/thymeleaf6
+    @GetMapping("/thymeleaf6")
+    public String getThymeleaf6(Model model) {
+
+        List<ProductDto> list = new ArrayList<>();
+        list.add(ProductDto.builder().productId(0L).productName("Product Name0").productPrice(2500).build());
+        list.add(ProductDto.builder().productId(1L).productName("Product Name1").productPrice(2501).build());
+        list.add(ProductDto.builder().productId(2L).productName("Product Name2").productPrice(2502).build());
+        list.add(ProductDto.builder().productId(3L).productName("Product Name3").productPrice(2503).build());
+
+
+        model.addAttribute("key_model3", list);
+        return "thymeleaf6";
     }
 }
