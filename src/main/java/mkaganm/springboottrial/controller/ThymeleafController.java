2177ac93dd9d2ac84739586e75d58d71badb194b
kagan.meric <mustafakaganmeric@gmail.com>
2023-05-02 03:48:10 +0300

    update

--- src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
+++ src/main/java/mkaganm/springboottrial/controller/ThymeleafController.java
@@ -3,9 +3,7 @@ package mkaganm.springboottrial.controller;
 import mkaganm.springboottrial.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
